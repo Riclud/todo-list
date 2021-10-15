043a6885c200ced427ebe7f219ffe61348398b4a
Riclud <riclud@mail.ru>
2021-10-15 16:22:15 +0800

    feat: typeorm module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.development.env', isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
   ],
 })
 export class AppModule {}
